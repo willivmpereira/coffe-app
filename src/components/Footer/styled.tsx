33a35c8d7eec8f3f02048627cf40332b8d30cdd0
brouwilliamog <william.pereira@focomultimidia.com>
2022-08-07 14:24:21 -0300

    :sparkles: feat: Footer

--- src/components/Footer/styled.tsx
+++ src/components/Footer/styled.tsx
@@ -1,1 +1,57 @@
 import styled from "styled-components"
+
+export const Footer = styled.footer`
+    padding: 3rem 0;
+    background-color: #855c57;
+`
+
+export const ContentFooter = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    
+    h2{
+        color: #FFF;
+    }
+`
+
+export const InfoFooter = styled.div`
+    display: flex;
+    gap: 5rem;
+
+    ul{
+        list-style: none;
+        padding: 0;
+        
+        li{
+            margin: 0.5rem 0;
+
+            a{
+                color: #FFF;
+                transition: 0.5s;   
+                text-decoration: none;
+            }
+        }
+    }
+`
+
+export const MenuFooter = styled.div`
+    ul{
+        list-style: none;
+        padding: 0;
+
+        li{
+            margin: .5rem 0;
+            a{
+                color: #FFF;
+                text-decoration: none;
+            }
+        }
+    }
+`
+export const copy = styled.div`
+    p{
+        text-align: center;
+        color: #FFF;
+    }
+`
