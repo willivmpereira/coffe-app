2fdb52e9e2bdc2470ce7f7a11e74c8c013396075
brouwilliamog <william.pereira@focomultimidia.com>
2022-08-07 13:15:21 -0300

    :sparkles: feat: Page Product

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,7 +1,42 @@
+import * as S from './styled'
+import { Container } from '../Container'
+import Image from 'next/image'
+import embalageCoffe from '../../../public/img/embalageCoffe.png'
+// import coffeAbout from '../../../public/img/coffe.png'
+// embalageCoffe.png
+
 export const Product = () => {
     return (
-        <section>
-            Produto
-        </section>
+        <S.Product>
+            <Container>
+            <S.Display>
+                <S.ContentInfo>
+                        <h3>Featured this week</h3>
+                        <h2>Coal Miner’s Organic</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci facilisis aliquam urna id neque viverra nunc,
+                            mauris, sollicitudin. Bibendum suscipit dictum elit, tellus dui, laoreet cursus. In parturient amet, urna nibh
+                            condimentum. Feugiat a pellentesque erat in. Aenean vulputate eu diam sit cursus lacus. Cursus urna elit
+                            donec neque platea vitae. Enim nisi in egestas in quis urna, tempus. Mi nec, id sit non. Placerat eu.
+                        </p>
+                        <a href="">Saiba mais
+                            <svg width="57" height="11" viewBox="0 0 57 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <line x1="4.37114e-08" y1="5.5" x2="49" y2="5.5" stroke="#ff8900"/>
+                                <path d="M57 5.5L48.75 10.2631L48.75 0.73686L57 5.5Z" fill="#ff8900"/>
+                            </svg>
+                        </a>
+                </S.ContentInfo>
+
+                <S.ContentImg>
+                    <Image 
+                        src={embalageCoffe} 
+                        alt=""  
+                        width={350} 
+                        height={573}
+                        />
+
+                </S.ContentImg>
+                </S.Display>
+            </Container>
+        </S.Product>
     )
 }
--- src/components/Product/styled.tsx
+++ src/components/Product/styled.tsx
@@ -1,1 +1,64 @@
 import styled from "styled-components";
+
+export const Product = styled.div`
+    height: 100%;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    margin: 2rem 0;
+`
+
+export const Display = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    align-items: end;
+    justify-content: center;
+    gap: 2rem;
+
+    @media screen and (max-width: 768px) {
+        grid-template-columns: 1fr;
+        gap: 0;
+    }
+`
+
+export const ContentInfo = styled.div`
+    h3{
+        font-weight: 500;
+        text-transform: uppercase;
+        font-size: 1.25rem;
+    }
+    h2{
+        font-weight: 400;
+        font-size: 2rem;
+        margin-top: 1.5rem;
+    }
+    p{
+        font-size: 0.85rem;
+        margin-top: 1.5rem;
+        line-height: 1.6;
+    }
+    a{
+        color: #ff8900;
+        text-decoration: none;
+        margin: 1.5rem 0 2rem;
+        display: inline-block;
+
+        &:hover{
+
+            svg{
+                margin-left: 1rem;
+            } 
+        }
+
+        svg{
+            transition: all 0.5s;
+            margin-left: 0.5rem;
+        }
+        
+    }
+`
+
+export const ContentImg = styled.div`
+    display: flex;
+    justify-content: center;
+`
