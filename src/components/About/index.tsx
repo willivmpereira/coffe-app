4916c707105aef66dd8139f1d737e8f78d177420
brouwilliamog <william.pereira@focomultimidia.com>
2022-08-07 12:34:31 -0300

    :sparkles: feat: Page AboutUs

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,7 +1,70 @@
+import * as S from './styled'
+import Image from 'next/image'
+import { Container } from '../Container'
+import coffeAbout from '../../../public/img/coffe.png'
+
 export const About = () => {
     return (
-        <section>
-            About
-        </section>
+        <S.AboutUs>
+            <Container>
+                <S.Display>
+                    <S.ContentImage>
+                        <Image 
+                            src={coffeAbout} 
+                            alt="Imagem Café" 
+                            
+                        />
+                    </S.ContentImage>
+                    
+                    <S.ContentText>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eleifend diam pretium eget semper. At
+                            orci at volutpat nunc sed. Fames diam urna, erat a, tristique lacus, at lectus. Sem lacinia sapien
+                            massa quisque maecenas ut. Eleifend turpis sed quam egestas. Bibendum tortor lorem sed
+                            ipsum. Ornare sollicitudin aliquet congue adipiscing aliquam vitae.
+                            
+                            Amet, phasellus nibh nisl, morbi purus. Vitae nisi, placerat proin in. Ultricies a dui, quam sit. Leo
+                            viverra ornare gravida diam, amet. Purus dui tortor in tellus bibendum et. Mattis massa vitae,
+                            sagittis, vitae cum. Maecenas morbi massa posuere eget libero imperdiet tincidunt. Blandit
+                            suscipit libero in tincidunt. Lobortis in urna ut sit amet mus non sapien dictum. Enim ullamcorper
+                            fusce risus egestas tincidunt quisque ultricies faucibus neque. Maecenas tincidunt lacus, sit
+                            eget amet arcu, nulla. Nisl et dui amet pellentesque rhoncus habitant suspendisse enim.
+                            Convallis eu, turpis amet, magna non ac. Hac massa eget massa, orci duis facilisis dui.
+                            
+                            Sit dolor habitasse volutpat tortor aliquam mauris massa. Amet nisl etiam ullamcorper
+                            suspendisse vitae mauris mattis ultricies. Integer quisque sagittis hendrerit nec amet tristique at
+                            nisi, pulvinar. Habitasse nam amet et sed. Molestie dapibus mattis faucibus risus. Ullamcorper
+                            vitae ullamcorper lorem gravida amet amet nulla.
+                        </p>
+                        <ul>
+                            <li>
+                                <S.ThumbImage
+                                    src={coffeAbout} 
+                                    alt="Imagem Café" 
+                                    width={100}
+                                    height={100}
+                                />
+                            </li>
+                            <li>
+                                <S.ThumbImage 
+                                    src={coffeAbout} 
+                                    alt="Imagem Café"
+                                    width={100}
+                                    height={100}
+                                />
+                            </li>
+                            <li>
+                                <S.ThumbImage 
+                                    src={coffeAbout} 
+                                    alt="Imagem Café"
+                                    width={100}
+                                    height={100}
+                                />
+                            </li>
+                        </ul>
+                    </S.ContentText>
+                </S.Display>
+            </Container>
+        </S.AboutUs>
     )
 }
--- src/components/About/styled.tsx
+++ src/components/About/styled.tsx
@@ -1,1 +1,55 @@
 import styled from "styled-components"
+import Image from 'next/image'
+
+export const AboutUs = styled.section`
+    background: linear-gradient(90deg, rgba(133,92,87,1) 0%, rgba(133,92,87,1) 35%, rgba(255,255,255,1) 35%, rgba(255,255,255,1) 100%);
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 3rem 0;
+
+    @media screen and (max-width: 768px) {
+        height: 100%;
+        margin: 4rem 0;
+        background: transparent;
+    }
+`
+
+export const Display = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    align-items: center;
+    gap: 2rem;
+
+    @media screen and (max-width: 768px) {
+        grid-template-columns: 1fr;
+    }
+`
+
+export const ContentImage = styled.div`
+    
+`
+export const ContentText = styled.div`
+    
+    p{
+        font-size: 0.9rem;
+    }
+
+    ul{
+        display: flex;
+        padding: 0;
+        list-style: none;
+        gap: 1rem;
+        margin-top: 2.5rem;
+
+        @media screen and (max-width: 768px) {
+            justify-content: center;
+        }
+
+    }
+`
+
+export const ThumbImage = styled(Image)`
+    border-radius: 50%;
+`
