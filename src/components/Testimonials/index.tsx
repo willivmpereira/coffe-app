4b83c5b499f3781b5ba42f590bc6894dcc24de38
brouwilliamog <william.pereira@focomultimidia.com>
2022-08-07 14:13:27 -0300

    :sparkles: feat: Page Testimonials

--- src/components/Testimonials/index.tsx
+++ src/components/Testimonials/index.tsx
@@ -1,7 +1,68 @@
+import * as S from './styled'
+import { Container } from '../Container'
+
+import PhotoCliente from '../../../public/img/fff.png'
+
 export const Testimonials = () => {
     return (
-        <section>
-            Testimonials
-        </section>
+        <S.Testimonials>
+            <Container>
+                <S.Title>Lorem ipsum dolor site</S.Title>
+                <S.SubTitle>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis</S.SubTitle>
+
+                <S.Display>
+                    <S.TestimonialContent>
+                        <S.DataTestimonial>
+                            <div>
+                                <S.ThumbImg 
+                                    src={PhotoCliente} 
+                                    width={100} 
+                                    height={100} 
+                                    alt="Foto do cliente" 
+                                />
+                            </div>
+                            <S.InfoTestimonial>
+                                <div>
+                                    <h4>Matt Robinson</h4>
+                                    <p>Client</p>
+                                </div>
+                                <div>
+                                    <ul>
+                                        <li>
+                                            <svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M9.99999 1.6665L12.575 6.88317L18.3333 7.72484L14.1667 11.7832L15.15 17.5165L9.99999 14.8082L4.84999 17.5165L5.83332 11.7832L1.66666 7.72484L7.42499 6.88317L9.99999 1.6665Z" fill="#FF8900" stroke="#FF8900" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                            </svg>
+                                        </li>
+                                        <li>
+                                            <svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M9.99999 1.6665L12.575 6.88317L18.3333 7.72484L14.1667 11.7832L15.15 17.5165L9.99999 14.8082L4.84999 17.5165L5.83332 11.7832L1.66666 7.72484L7.42499 6.88317L9.99999 1.6665Z" fill="#FF8900" stroke="#FF8900" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                            </svg>
+                                        </li>
+                                        <li>
+                                            <svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M9.99999 1.6665L12.575 6.88317L18.3333 7.72484L14.1667 11.7832L15.15 17.5165L9.99999 14.8082L4.84999 17.5165L5.83332 11.7832L1.66666 7.72484L7.42499 6.88317L9.99999 1.6665Z" fill="#FF8900" stroke="#FF8900" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                            </svg>
+                                        </li>
+                                        <li>
+                                            <svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M9.99999 1.6665L12.575 6.88317L18.3333 7.72484L14.1667 11.7832L15.15 17.5165L9.99999 14.8082L4.84999 17.5165L5.83332 11.7832L1.66666 7.72484L7.42499 6.88317L9.99999 1.6665Z" fill="#FF8900" stroke="#FF8900" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                            </svg>
+                                        </li>
+                                        <li>
+                                            <svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M9.99999 1.6665L12.575 6.88317L18.3333 7.72484L14.1667 11.7832L15.15 17.5165L9.99999 14.8082L4.84999 17.5165L5.83332 11.7832L1.66666 7.72484L7.42499 6.88317L9.99999 1.6665Z" fill="#FF8900" stroke="#FF8900" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                            </svg>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </S.InfoTestimonial>
+                        </S.DataTestimonial>
+                        <S.TextTestimonial>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id porttitor nullam ullamcorper sit in tincidunt massa erat faucibus. Malesuada dignissim consequat.</p>
+                        </S.TextTestimonial>
+                    </S.TestimonialContent>
+                </S.Display>
+            </Container>
+        </S.Testimonials>
     )
 }
--- src/components/Testimonials/styled.tsx
+++ src/components/Testimonials/styled.tsx
@@ -1,1 +1,71 @@
 import styled from "styled-components";
+import Image from 'next/image'
+
+export const Testimonials = styled.section`
+    padding: 4rem 0;
+`
+
+export const Title = styled.h2`
+    text-align: center;
+    margin: 0;
+`
+
+export const SubTitle = styled.p`
+    text-align: center;
+    margin-top: 0;
+`
+
+export const Display = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    margin-top: 5rem;
+
+    @media(max-width: 768px) {
+        grid-template-columns: 1fr;
+    }
+`
+
+export const TestimonialContent = styled.div`
+    margin: 2rem 0;
+`
+
+export const DataTestimonial = styled.div`
+    display: flex;
+    gap: 0.5rem;
+    align-items: center;
+`
+
+export const ThumbImg = styled(Image)`
+    border-radius: 50%;
+`
+
+export const InfoTestimonial = styled.div`
+    display: flex;
+    align-items: flex-start;
+    gap: .5rem;
+
+    h4{
+        margin: 0;
+    }
+
+    p{
+        text-align: left;
+        margin-top: 0.5rem;
+    }
+
+    ul{
+        display: flex;
+        padding: 0;
+        margin: 0;
+        list-style: none;
+        gap: .25rem;
+    }
+`
+
+export const TextTestimonial = styled.div`
+    p{
+        font-size: 0.85rem;
+        margin-top: .5rem;
+        text-align: left;
+    }
+`
