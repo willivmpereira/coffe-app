04defa2b231e70e9c6bfda94996dded68c26b94f
brouwilliamog <william.pereira@focomultimidia.com>
2022-08-07 14:38:04 -0300

    :sparkles: feat: Page Contact

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,7 +1,66 @@
+import * as S from './styled'
+import { Container } from '../Container'
+
+
 export const Contact = () => {
     return (
-        <section>
-            Contato
-        </section>
+        <S.Contact>
+            <Container>
+                <h2>Contato</h2>
+                <S.Display>
+                    <form action="">
+                        <S.FormGroup>
+                            <label>Nome</label>
+                            <input type="text" id="name" name='name'/>
+                        </S.FormGroup>
+                        <S.FormGroup>
+                            <label>Telefone</label>
+                            <input type="text"/>
+                        </S.FormGroup>
+                        <S.FormGroup>
+                            <label>E-mail</label>
+                            <input type="email"/>
+                        </S.FormGroup>  
+                        <S.FormGroup>
+                            <label>Mensagem</label>
+                            <textarea name="mensagem" id=""></textarea>
+                        </S.FormGroup>
+                        <S.ButtonSubmit type="submit">Enviar mensagem
+                            <svg width="57" height="11" viewBox="0 0 57 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <line x1="4.37114e-08" y1="5.5" x2="49" y2="5.5" stroke="white"/>
+                                <path d="M57 5.5L48.75 10.2631L48.75 0.73686L57 5.5Z" fill="white"/>
+                            </svg>
+                        </S.ButtonSubmit>
+                    </form>
+                    <S.ContactInfo>
+                        <p>Estamos apenas aguardando o seu contato,
+                            que pode ser feito pelo formulário, e-mail, telefone ou WhatsApp.</p>
+
+                        <ul>
+                            <li>
+                                <a href="#">contato@seusite.com</a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    (55) 11 90000-0000
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    (55) 11 90000-0000
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                   <p>Av Paulista, 000</p>
+                                    <p>Bela Vista, São Paulo - SP</p>
+                                    <p>CEP: 00000-000, CoWorking</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </S.ContactInfo>
+                </S.Display>
+            </Container>
+        </S.Contact>
     )
 }
--- src/components/Contact/styled.tsx
+++ src/components/Contact/styled.tsx
@@ -1,1 +1,82 @@
 import styled from "styled-components"
+
+export const Contact = styled.section`
+    padding: 3rem 0;
+    background-color: #E5E5E5;
+`
+
+export const Display = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 2rem;
+
+    @media(max-width: 768px) {
+        grid-template-columns: 1fr;
+    }
+`
+
+export const FormGroup = styled.div`
+        margin: 1rem 0;
+
+
+        input{
+            display: block;
+            width: 100%;
+            font-size: 1rem;
+            padding: 0.5rem;
+            border-radius: .25rem;
+            border: 1px solid black;
+            margin-top: 0.25rem;
+        }
+        textarea{
+            display: block;
+            width: 100%;
+            font-size: 1rem;
+            padding: .5rem;
+            border-radius: .25rem;
+            border: 1px solid black;
+        }
+`
+
+export const ButtonSubmit = styled.button`
+    background-color: #ff8900;
+    color: #FFF;
+    border: none;
+    padding: 0.75rem 2rem;
+    display: flex;
+    align-items: center;
+    width: 300px;
+    border-radius: 4px;
+    cursor: pointer;
+
+    &:hover{
+
+        svg{
+            margin-left: 1rem;
+        } 
+    }
+    
+    svg{
+        transition: all 0.5s;
+        margin-left: .5rem;
+    }
+`
+
+export const ContactInfo = styled.div`
+    margin-top: 1rem;
+
+    ul{
+        list-style: none;
+        padding: 0;
+
+        li{
+            margin: 1rem 0;
+
+            a{
+                color: #000;
+                display: inline-block;
+                text-decoration: none;
+            }
+        }
+    }
+`
