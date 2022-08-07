135d923deb7fc95014ac1fb2d61b8e831d0be576
brouwilliamog <william.pereira@focomultimidia.com>
2022-08-07 13:33:58 -0300

    :sparkles: feat: Page Qualities

--- src/components/Qualities/index.tsx
+++ src/components/Qualities/index.tsx
@@ -1,7 +1,58 @@
+import * as S from './styled'
+import { Container } from '../Container'
+import Imagem from 'next/image'
+import embalageCoffe from '../../../public/img/embalageCoffe.png'
+
 export const Qualities = () => {
     return (
-        <section>
-            Qualities
-        </section>
+        <S.Qualities>
+            <Container>
+                <S.Title>Lorem ipsun dolor site</S.Title>
+                <S.Subtitle>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam</S.Subtitle>
+                <S.Display>
+                    <S.ContentInfoOne>
+                        <ul>
+                            <li>
+                                <h5>The Perfect Coffe</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing
+                                    elit. In eget tempus tortor sagittis suspendisse.</p>
+                            </li>
+                            <li>
+                                <h5>The Perfect Coffe</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing
+                                    elit. In eget tempus tortor sagittis suspendisse.</p>
+                            </li>
+                            <li>
+                                <h5>The Perfect Coffe</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing
+                                    elit. In eget tempus tortor sagittis suspendisse.</p>
+                            </li>
+                        </ul>
+                    </S.ContentInfoOne>
+                    <S.ContentImg>
+                        <Imagem src={embalageCoffe} alt="Embalagem do Café" width={250} height={409}/>
+                    </S.ContentImg>
+                    <S.ContentInfoTwo>
+                        <ul>
+                            <li>
+                                <h5>The Perfect Coffe</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing
+                                    elit. In eget tempus tortor sagittis suspendisse.</p>
+                            </li>
+                            <li>
+                                <h5>The Perfect Coffe</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing
+                                    elit. In eget tempus tortor sagittis suspendisse.</p>
+                            </li>
+                            <li>
+                                <h5>The Perfect Coffe</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing
+                                    elit. In eget tempus tortor sagittis suspendisse.</p>
+                            </li>
+                        </ul>
+                    </S.ContentInfoTwo>
+                </S.Display>
+            </Container>
+        </S.Qualities>
     )
 }
--- src/components/Qualities/styled.tsx
+++ src/components/Qualities/styled.tsx
@@ -1,1 +1,106 @@
 import styled from "styled-components";
+
+export const Qualities = styled.div`
+    height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #e9e2da;
+    padding: 3rem 0;
+`
+
+export const Display = styled.div`
+    display: grid;
+    grid-template-columns: 1fr .5fr 1fr;
+    gap: 1.5rem;
+    justify-content: center;
+    align-items: center;
+    margin-top: 3rem;
+
+    @media(max-width: 768px) {
+        grid-template-columns: 1fr;
+    }
+`
+
+export const Title = styled.h2`
+    text-align: center;
+    margin: 0;
+`
+
+export const Subtitle = styled.p`
+    text-align: center;
+    margin-top: 0;
+`
+
+export const ContentInfoOne = styled.div`
+    text-align: right;
+
+    @media(max-width: 768px) {
+        text-align: left;
+    }
+
+    ul{
+        list-style: none;
+        padding: 0;
+        
+        li{
+            margin: 1.5rem 1.5rem;
+
+            h5{
+                margin: 0;
+            }
+            
+            p{
+                font-size: .85rem;
+                margin-top: 0.25rem;
+                position: relative;
+            }
+
+            &:nth-of-type(2){
+                margin-right: 3.5rem;
+            }
+        }
+    }
+`
+
+export const ContentImg = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+export const ContentInfoTwo = styled.div`
+    text-align: left;
+
+    @media(max-width: 768px) {
+        text-align: left;
+    }
+
+    ul{
+        list-style: none;
+        padding: 0;
+
+        h5{
+            margin: 0;
+        }
+        
+        li{
+            margin: 1.5rem 1.5rem;
+            
+            p{
+                font-size: .85rem;
+                margin-top: 0.25rem;
+                position: relative;
+            }
+
+            &:nth-of-type(2){
+                margin-left: 3.5rem;
+
+                @media(max-width: 768px){
+                    margin-left: 1.5rem;
+                }
+            }
+        }
+    }
+`
